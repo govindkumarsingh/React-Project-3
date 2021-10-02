61d5f19f73be7c048eebf4b77c2ad23eee356262
gosingh <Govind.Singh@radisys.com>
2021-10-02 22:20:14 +0530

    added a custom context provider component

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogin: (email, password) => {},
+  onLogOut: () => {}
 })
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(() => {
+    if (localStorage.getItem('isLoggedIn') === '1') {
+      setIsLoggedIn(true)
+    }
+  }, [])
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn')
+    setIsLoggedIn(false)
+  }
+
+  const loginHandler = () => {
+    localStorage.setItem('isLoggedIn', '1')
+    setIsLoggedIn(true)
+  }
+
+  return <AuthContext.Provider value={{ isLoggedIn: isLoggedIn, onLogin: loginHandler, onLogout: logoutHandler }}>{props.children}</AuthContext.Provider>
+}
 
 export default AuthContext;
