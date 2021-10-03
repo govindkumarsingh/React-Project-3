76095f5339e68e9d2fb058a5e688b4a42b25df26
gosingh <Govind.Singh@radisys.com>
2021-10-03 15:12:33 +0530

    formatted home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,9 @@
-import React, { useContext } from 'react';
-import AuthContext from '../../store/auth-context';
-import Button from '../UI/Button/Button';
+import React, { useContext } from 'react'
+import AuthContext from '../../store/auth-context'
+import Button from '../UI/Button/Button'
 
-import Card from '../UI/Card/Card';
-import classes from './Home.module.css';
+import Card from '../UI/Card/Card'
+import classes from './Home.module.css'
 
 const Home = () => {
   const authCtx = useContext(AuthContext)
@@ -13,7 +13,7 @@ const Home = () => {
       <h1>Welcome back!</h1>
       <Button onClick={authCtx.onLogout}>Logout</Button>
     </Card>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
